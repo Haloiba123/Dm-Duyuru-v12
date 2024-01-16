732b71497d98bf13cc3fa794a2f583e1cfcfc63b
Haloiba123 <151461965+Haloiba123@users.noreply.github.com>
2024-01-16 17:26:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";//istediğiniz gibi ayarlayabilirsiniz prefixi
+var prefix = "!";//istediğiniz gibi ayarlayabilirsiniz prefixi
 var statuses = [`Altyapı Videosu İçin youtube.com/@WraithsDev`];
 var timers = 2;
-const owners = ["sahip_id"];//botu kullanmak için sahip_id girmeyi unutmayın
+const owners = ["901790074868477953"];//botu kullanmak için sahip_id girmeyi unutmayın
 
 client.on("ready", () => {
   console.log(`Giriş Yapıldı: ${client.user.tag}`);
@@ -157,4 +157,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(`bottokengir`);
+client.login(`MTE5NjgxODYyODEyNTkzNzc4Nw.G6lNfW.90mWsWueR3QsjdJVtGOtj-JRlm4HiCYYefQVv8`);
